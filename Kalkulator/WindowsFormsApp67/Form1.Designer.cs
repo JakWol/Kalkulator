3b679a7567d6ee669c59ef501c987d04cc60b1b2
JakWol <58342272+JakWol@users.noreply.github.com>
2022-02-20 20:36:49 +0100

    Add files via upload

---
+++ Kalkulator/WindowsFormsApp67/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace WindowsFormsApp67
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.jeden = new System.Windows.Forms.Button();
+            this.cztery = new System.Windows.Forms.Button();
+            this.siedem = new System.Windows.Forms.Button();
+            this.dwa = new System.Windows.Forms.Button();
+            this.piec = new System.Windows.Forms.Button();
+            this.osiem = new System.Windows.Forms.Button();
+            this.trzy = new System.Windows.Forms.Button();
+            this.szesc = new System.Windows.Forms.Button();
+            this.dziewiec = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.rownaSie = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.mnozenie = new System.Windows.Forms.Button();
+            this.dzielenie = new System.Windows.Forms.Button();
+            this.przecinek = new System.Windows.Forms.Button();
+            this.pierwiastek = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(89, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(375, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // jeden
+            // 
+            this.jeden.Location = new System.Drawing.Point(89, 153);
+            this.jeden.Name = "jeden";
+            this.jeden.Size = new System.Drawing.Size(75, 23);
+            this.jeden.TabIndex = 1;
+            this.jeden.Text = "1";
+            this.jeden.UseVisualStyleBackColor = true;
+            this.jeden.Click += new System.EventHandler(this.jeden_Click);
+            // 
+            // cztery
+            // 
+            this.cztery.Location = new System.Drawing.Point(89, 203);
+            this.cztery.Name = "cztery";
+            this.cztery.Size = new System.Drawing.Size(75, 23);
+            this.cztery.TabIndex = 2;
+            this.cztery.Text = "4";
+            this.cztery.UseVisualStyleBackColor = true;
+            this.cztery.Click += new System.EventHandler(this.cztery_Click);
+            // 
+            // siedem
+            // 
+            this.siedem.Location = new System.Drawing.Point(89, 259);
+            this.siedem.Name = "siedem";
+            this.siedem.Size = new System.Drawing.Size(75, 23);
+            this.siedem.TabIndex = 3;
+            this.siedem.Text = "7";
+            this.siedem.UseVisualStyleBackColor = true;
+            this.siedem.Click += new System.EventHandler(this.siedem_Click);
+            // 
+            // dwa
+            // 
+            this.dwa.Location = new System.Drawing.Point(242, 153);
+            this.dwa.Name = "dwa";
+            this.dwa.Size = new System.Drawing.Size(75, 23);
+            this.dwa.TabIndex = 4;
+            this.dwa.Text = "2";
+            this.dwa.UseVisualStyleBackColor = true;
+            this.dwa.Click += new System.EventHandler(this.dwa_Click);
+            // 
+            // piec
+            // 
+            this.piec.Location = new System.Drawing.Point(242, 203);
+            this.piec.Name = "piec";
+            this.piec.Size = new System.Drawing.Size(75, 23);
+            this.piec.TabIndex = 5;
+            this.piec.Text = "5";
+            this.piec.UseVisualStyleBackColor = true;
+            this.piec.Click += new System.EventHandler(this.piec_Click);
+            // 
+            // osiem
+            // 
+            this.osiem.Location = new System.Drawing.Point(242, 259);
+            this.osiem.Name = "osiem";
+            this.osiem.Size = new System.Drawing.Size(75, 23);
+            this.osiem.TabIndex = 6;
+            this.osiem.Text = "8";
+            this.osiem.UseVisualStyleBackColor = true;
+            this.osiem.Click += new System.EventHandler(this.osiem_Click);
+            // 
+            // trzy
+            // 
+            this.trzy.Location = new System.Drawing.Point(409, 153);
+            this.trzy.Name = "trzy";
+            this.trzy.Size = new System.Drawing.Size(75, 23);
+            this.trzy.TabIndex = 7;
+            this.trzy.Text = "3";
+            this.trzy.UseVisualStyleBackColor = true;
+            this.trzy.Click += new System.EventHandler(this.trzy_Click);
+            // 
+            // szesc
+            // 
+            this.szesc.Location = new System.Drawing.Point(409, 203);
+            this.szesc.Name = "szesc";
+            this.szesc.Size = new System.Drawing.Size(75, 23);
+            this.szesc.TabIndex = 8;
+            this.szesc.Text = "6";
+            this.szesc.UseVisualStyleBackColor = true;
+            this.szesc.Click += new System.EventHandler(this.szesc_Click);
+            // 
+            // dziewiec
+            // 
+            this.dziewiec.Location = new System.Drawing.Point(409, 259);
+            this.dziewiec.Name = "dziewiec";
+            this.dziewiec.Size = new System.Drawing.Size(75, 23);
+            this.dziewiec.TabIndex = 9;
+            this.dziewiec.Text = "9";
+            this.dziewiec.UseVisualStyleBackColor = true;
+            this.dziewiec.Click += new System.EventHandler(this.dziewiec_Click);
+            // 
+            // zero
+            // 
+            this.zero.Location = new System.Drawing.Point(242, 317);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(75, 23);
+            this.zero.TabIndex = 10;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // rownaSie
+            // 
+            this.rownaSie.Location = new System.Drawing.Point(522, 65);
+            this.rownaSie.Name = "rownaSie";
+            this.rownaSie.Size = new System.Drawing.Size(75, 23);
+            this.rownaSie.TabIndex = 11;
+            this.rownaSie.Text = "=";
+            this.rownaSie.UseVisualStyleBackColor = true;
+            this.rownaSie.Click += new System.EventHandler(this.rownaSie_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(522, 120);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(75, 23);
+            this.plus.TabIndex = 12;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(647, 119);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(75, 23);
+            this.minus.TabIndex = 13;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(647, 65);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 14;
+            this.clear.Text = "ce";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // mnozenie
+            // 
+            this.mnozenie.Location = new System.Drawing.Point(522, 179);
+            this.mnozenie.Name = "mnozenie";
+            this.mnozenie.Size = new System.Drawing.Size(75, 23);
+            this.mnozenie.TabIndex = 15;
+            this.mnozenie.Text = "*";
+            this.mnozenie.UseVisualStyleBackColor = true;
+            this.mnozenie.Click += new System.EventHandler(this.mnozenie_Click);
+            // 
+            // dzielenie
+            // 
+            this.dzielenie.Location = new System.Drawing.Point(647, 178);
+            this.dzielenie.Name = "dzielenie";
+            this.dzielenie.Size = new System.Drawing.Size(75, 23);
+            this.dzielenie.TabIndex = 16;
+            this.dzielenie.Text = "/";
+            this.dzielenie.UseVisualStyleBackColor = true;
+            this.dzielenie.Click += new System.EventHandler(this.dzielenie_Click);
+            // 
+            // przecinek
+            // 
+            this.przecinek.Location = new System.Drawing.Point(409, 316);
+            this.przecinek.Name = "przecinek";
+            this.przecinek.Size = new System.Drawing.Size(75, 23);
+            this.przecinek.TabIndex = 17;
+            this.przecinek.Text = ".";
+            this.przecinek.UseVisualStyleBackColor = true;
+            this.przecinek.Click += new System.EventHandler(this.przecinek_Click);
+            // 
+            // pierwiastek
+            // 
+            this.pierwiastek.Location = new System.Drawing.Point(522, 229);
+            this.pierwiastek.Name = "pierwiastek";
+            this.pierwiastek.Size = new System.Drawing.Size(75, 23);
+            this.pierwiastek.TabIndex = 18;
+            this.pierwiastek.Text = "sqrRt";
+            this.pierwiastek.UseVisualStyleBackColor = true;
+            this.pierwiastek.Click += new System.EventHandler(this.pierwiastek_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pierwiastek);
+            this.Controls.Add(this.przecinek);
+            this.Controls.Add(this.dzielenie);
+            this.Controls.Add(this.mnozenie);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.rownaSie);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.dziewiec);
+            this.Controls.Add(this.szesc);
+            this.Controls.Add(this.trzy);
+            this.Controls.Add(this.osiem);
+            this.Controls.Add(this.piec);
+            this.Controls.Add(this.dwa);
+            this.Controls.Add(this.siedem);
+            this.Controls.Add(this.cztery);
+            this.Controls.Add(this.jeden);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button jeden;
+        private System.Windows.Forms.Button cztery;
+        private System.Windows.Forms.Button siedem;
+        private System.Windows.Forms.Button dwa;
+        private System.Windows.Forms.Button piec;
+        private System.Windows.Forms.Button osiem;
+        private System.Windows.Forms.Button trzy;
+        private System.Windows.Forms.Button szesc;
+        private System.Windows.Forms.Button dziewiec;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button rownaSie;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button mnozenie;
+        private System.Windows.Forms.Button dzielenie;
+        private System.Windows.Forms.Button przecinek;
+        private System.Windows.Forms.Button pierwiastek;
+    }
+}
+
---
+++ Kalkulator/WindowsFormsApp67/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator
+{
+    public partial class Form1 : Form
+    {
+        
+
+
+        public double? liczbaA = null;
+        public double? liczbaB = null;
+        public double? wynik = null;
+        public char? znakOperacji=null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private Boolean textBox_disableInput()
+        {
+            if (textBox1.Text.Length == 6)
+            {
+                textBox1.Text = textBox1.Text;
+                return false;
+            }
+            return true;
+        }
+
+        private void jeden_Click(object sender, EventArgs e)
+        {
+            if (textBox_disableInput() == true)
+            {
+                textBox1.Text = textBox1.Text + "1";
+            }
+            
+            
+        }
+
+        private void dwa_Click(object sender, EventArgs e)
+        {
+            if (textBox_disableInput() == true)
+            {
+                textBox1.Text = textBox1.Text + "2";
+            }
+        }
+
+        private void trzy_Click(object sender, EventArgs e)
+        {
+            if (textBox_disableInput() == true)
+            {
+                textBox1.Text = textBox1.Text + "3";
+            }
+        }
+
+        private void cztery_Click(object sender, EventArgs e)
+        {
+            if (textBox_disableInput() == true)
+            {
+                textBox1.Text = textBox1.Text + "4";
+            }
+        }
+
+        private void piec_Click(object sender, EventArgs e)
+        {
+            if (textBox_disableInput() == true)
+            {
+                textBox1.Text = textBox1.Text + "5";
+            }
+        }
+
+        private void szesc_Click(object sender, EventArgs e)
+        {
+            if (textBox_disableInput() == true)
+            {
+                textBox1.Text = textBox1.Text + "6";
+            }
+        }
+
+        private void siedem_Click(object sender, EventArgs e)
+        {
+            if (textBox_disableInput() == true)
+            {
+                textBox1.Text = textBox1.Text + "7";
+            }
+        }
+
+        private void osiem_Click(object sender, EventArgs e)
+        {
+            if (textBox_disableInput() == true)
+            {
+                textBox1.Text = textBox1.Text + "8";
+            }
+        }
+
+        private void dziewiec_Click(object sender, EventArgs e)
+        {
+            if (textBox_disableInput() == true)
+            {
+                textBox1.Text = textBox1.Text + "9";
+            }
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            if (textBox_disableInput() == true)
+            {
+                textBox1.Text = textBox1.Text + "0";
+            }
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "" && liczbaA == null)
+            {
+                textBox1.Text = textBox1.Text;
+                znakOperacji = null;
+            } else if (znakOperacji == '*' || znakOperacji == '/' || znakOperacji == '+' || znakOperacji == '-')
+            {
+                textBox1.Text = "" + wynik;
+                znakOperacji = null;
+            } else 
+            liczbaA = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+            znakOperacji = '+';
+
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" && liczbaA == null)
+            {
+                textBox1.Text = textBox1.Text;
+                znakOperacji = null;
+            }
+            else if(znakOperacji == '*' || znakOperacji == '/' || znakOperacji == '+' || znakOperacji == '-')
+            {
+                textBox1.Text = "" + wynik;
+                znakOperacji = null;
+            }
+            else
+            liczbaA = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+            znakOperacji = '-';
+        }
+
+        private void rownaSie_Click(object sender, EventArgs e)
+        {
+
+            if(textBox1.Text == "")
+            {
+                textBox1.Text = textBox1.Text + wynik;
+                znakOperacji = null;
+            } else
+
+             switch(znakOperacji)
+            {
+                case '+':
+
+                    liczbaB = Convert.ToDouble(textBox1.Text);
+                    wynik = liczbaA + liczbaB;
+                    textBox1.Text = wynik+"";
+                    znakOperacji = null;
+                    break;
+                case '-':
+                    liczbaB = Convert.ToDouble(textBox1.Text);
+                    wynik = liczbaA - liczbaB;
+                    textBox1.Text = wynik + "";
+                    znakOperacji = null;
+                    break;
+                case '*':
+                    liczbaB = Convert.ToDouble(textBox1.Text);
+                    wynik = liczbaA * liczbaB;
+                    textBox1.Text = wynik + "";
+                    znakOperacji = null;
+                    break;
+                case '/':
+                    
+                    liczbaB = Convert.ToDouble(textBox1.Text);
+                    if (liczbaB == 0)
+                    {
+                        MessageBox.Show("Pamietaj megiero, nie dziel przez zero");
+                        break;
+                    }
+                    wynik = liczbaA / liczbaB;
+                    textBox1.Text = wynik + "";
+                    znakOperacji = null;
+                    break;
+
+                default:
+                    textBox1.Text = wynik + "" ;
+                    break;
+
+            }
+            
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            liczbaA = null;
+            liczbaB = null;
+            wynik = null;
+        }
+
+        private void mnozenie_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" && liczbaA == null)
+            {
+                textBox1.Text = textBox1.Text;
+                znakOperacji = null;
+            }
+            else if (znakOperacji == '*'|| znakOperacji == '/' || znakOperacji == '+' || znakOperacji == '-')
+            {
+                textBox1.Text = "" + wynik;
+                znakOperacji = null;
+            }
+            else
+                liczbaA = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+            znakOperacji = '*';
+        }
+
+        private void dzielenie_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" && liczbaA == null)
+            {
+                textBox1.Text = textBox1.Text;
+                znakOperacji = null;
+            }
+            else if (znakOperacji == '*' || znakOperacji == '/' || znakOperacji == '+' || znakOperacji == '-')
+            {
+                textBox1.Text = "" + wynik;
+                znakOperacji = null;
+            }
+            else
+                
+            liczbaA = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+            znakOperacji = '/';
+        }
+
+        private void przecinek_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains('.'))
+            {
+                textBox1.Text = textBox1.Text;
+            } else if (textBox1.Text == "")
+            {
+                textBox1.Text = "";
+            }else
+            textBox1.Text = textBox1.Text + '.';
+            
+            
+        }
+
+        private void pierwiastek_Click(object sender, EventArgs e)
+        {
+         
+            if (textBox1.Text == "" && liczbaA == null)
+            {
+                MessageBox.Show("nullll");
+                textBox1.Text = textBox1.Text;
+                znakOperacji = null;
+            }
+            else if (liczbaA <= 0)
+            {
+
+                MessageBox.Show("Nie moge wykonac pierwiastka z liczby " + textBox1.Text);
+            }
+            else if (textBox1.Text != "")
+            {
+                liczbaA = Convert.ToDouble(textBox1.Text);
+                liczbaA = Math.Sqrt(liczbaA.Value);
+                textBox1.Text = liczbaA + "";
+            }
+        }
+    }
+}
